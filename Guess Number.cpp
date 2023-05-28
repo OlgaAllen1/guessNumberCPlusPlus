c1a875a30408f2e3bc151a509ca02fa83148247c
Olga Allen <32139467+OlgaAllen1@users.noreply.github.com>
2023-05-28 12:08:53 -0600

    project finished

--- Guess Number.cpp
+++ Guess Number.cpp
@@ -1,20 +1,29 @@
-// Guess Number.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int randomNumber, userNumber;
+	cout << "Welcome to Guess a Number\n";
+	cout << "I'm thinking of a number between 1 and 100\n";
+	randomNumber = rand() % 100 + 1;
+	while (true) {
+		cout << "What is your guess? ";
+		cin >> userNumber;
+		if (userNumber > randomNumber) {
+			cout << "The guess is too high\n";
+		}
+		else if (userNumber < randomNumber) {
+			cout << "The guess is too low\n";
+		}
+		else {
+			cout << "That guess is just right!\n";
+			cout << "Thanks for playing\n";
+			break;
+		}
+	}
+	
+
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
